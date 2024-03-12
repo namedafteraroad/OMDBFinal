b5aa9d5496631a0c59dbcd4dd7c5f0a64feb5dca
will street <will@williamdstreet.com>
2024-03-12 15:20:13 -0500

    final

---
+++ True_Coders_Final_William_Street/OMDBFinal/OMDBFinal/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMDBFinal.Models;
+using Newtonsoft.Json;
+
+namespace OMDBFinal.Controllers
+{
+    public class MovieController : Controller
+    {
+        public async Task<IActionResult> Search(string title)
+        {
+            var client = new HttpClient();
+            var apiKey = "286aaf38";
+            var response = await client.GetStringAsync($"http://www.omdbapi.com/?t={title}&apikey={apiKey}");
+            var movie = JsonConvert.DeserializeObject<Movie>(response);
+            return View(movie);
+        }
+    }
+}
---
+++ True_Coders_Final_William_Street/OMDBFinal/OMDBFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OMDBFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
